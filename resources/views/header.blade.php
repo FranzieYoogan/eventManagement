09172e2c509a6fef5596f021551369b9f1ac06c7
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-05-04 18:51:55 -0300

    dashboard

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -56,6 +56,11 @@
             <a href="/dashboard" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{{session('admin')}}</a>
           </li>
 
+          
+          <li>
+            <a href="/createevent" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Create Event</a>
+          </li>
+
           <li>
             <a href="/logout" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">logout</a>
           </li>
