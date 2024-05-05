aba9bb2dd28421fbbcd4a592b7ffc14d73b16bb0
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-05-05 18:13:23 -0300

    moreDesign

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -45,7 +45,7 @@
             @if (!session()->has('admin'))
 
             <li>
-                <a href="/" class="linkStyle block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Login</a>
+                <a href="/" class="linkStyle block py-2 px-3 text-gray-900 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">Login</a>
               </li>
                 
             @else
